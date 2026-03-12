5860df1e1b28181c1828cfd584d4c57bc89062d3
Nicolas <nicolascesarpauferro@gmail.com>
2026-03-11 22:00:22 -0300

    Turning Excalidraw compatible with android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.edsonmatematico.benehimedraw',
+  appName: 'BenehimeDraw',
+  webDir: 'excalidraw-app/build'
+};
+
+export default config;
